3c5bdf69ad75df9772f02a374dab93e9f6a161e2
ADEJUHANAS <adejuhanas@studenr.uir.ac.id>
2022-01-09 19:51:59 +0700

    switch case

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-console.log ("if else");
-var nilai = prompt ("masukkan nilai 1 sampai 10");
-console.log (nilai);
-if (nilai > 5) {
-    console.log ("lulus");
+var pilih = prompt ("masukkan pilihan"); 
+switch (pilih){
+    case '1':
+        console.log ("if else");
+        var nilai = prompt ("masukkan nilai 1 sampai 10");
+        console.log (nilai);
+        if (nilai > 5) {
+            console.log ("lulus");
+        
+        } else{console.log ("tidak lulus")} 
+        break;
+        
+}
 
-} else{console.log ("tidak lulus")} 
+
+
 

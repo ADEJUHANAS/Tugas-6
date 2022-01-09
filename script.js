bda153a901c3e74fd8b7069e8201b96012675570
ADEJUHANAS <adejuhanas@studenr.uir.ac.id>
2022-01-09 19:42:35 +0700

    if else

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log ("if else");
+var nilai = prompt ("masukkan nilai 1 sampai 10");
+console.log (nilai);
+if (nilai > 5) {
+    console.log ("lulus");
+
+} else{console.log ("tidak lulus")} 
+
